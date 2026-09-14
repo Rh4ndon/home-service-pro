a859cc1fe5a110e621727b8e90a0f7e9c8c6b2a4
Rh4ndon <daverhandon@gmail.com>
2026-09-15 00:49:14 +0800

    Update db conn for deployment

--- models/dbconn.php
+++ models/dbconn.php
@@ -1,4 +1,5 @@
 <?php
+//$conn = mysqli_connect('localhost', 'u892177228_usr_IBE5aRna', 'Fl3$/zjsm', 'u892177228_db_IBE5aRna') or die('Connection Failed');
 $conn = mysqli_connect('localhost', 'root', '', 'home_service_pro') or die('Connection Failed');
 mysqli_set_charset($conn, "utf8mb4");
 date_default_timezone_set('Asia/Manila');
